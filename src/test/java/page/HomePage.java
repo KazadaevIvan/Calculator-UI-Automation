d750c717b2a44547495251d71b221ec714a1c4c0
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-18 11:42:40 -0700

    Implement test script

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -1,11 +1,42 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import waits.CustomConditions;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePage extends AbstractPage {
 
     private final String BASE_URL = "https://web2.0calc.com/";
+
+    @FindBy(xpath = "//button[@value='consent']")
+    private WebElement consentButton;
+
+    @FindBy(xpath = "//button[@aria-label='Consent']")
+    private WebElement consentButton2;
+
+    @FindBy(id = "input")
+    private WebElement expressionInput;
+
+    @FindBy(id = "trigorad")
+    private WebElement radRadioButton;
+
+    @FindBy(id = "BtnCalc")
+    private WebElement equalSignButton;
+
+    @FindBy(id = "hist")
+    private WebElement historyDropdown;
+
+    @FindBy(xpath = "//p[@data-inp]")
+    List<WebElement> historyRecordsList;
+
+    private String expression;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -16,5 +47,66 @@ public class HomePage extends AbstractPage {
     public HomePage openPage() {
         driver.navigate().to(BASE_URL);
         return this;
+    }
+
+    public HomePage waitForPageLoad() {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until(CustomConditions.jQueryAJAXsCompleted());
+        return this;
+    }
+
+    public HomePage consentManagementPlatformLoading() {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(consentButton));
+        consentButton.click();
+        return this;
+    }
+
+    public HomePage consentPersonalDataUsage() {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(consentButton2));
+        consentButton2.click();
+        return this;
+    }
+
+    public HomePage inputExpression(String expression) {
+        this.expression = expression;
+        expressionInput.sendKeys(expression);
+        return this;
+    }
+
+    public HomePage clickRadRadioButton() {
+        radRadioButton.click();
+        return this;
+    }
+
+    public HomePage clickEqualSignButton() {
+        equalSignButton.click();
+        return this;
+    }
+
+    public HomePage waitForExpressionInputValueUpdate() {
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.not(ExpectedConditions.textToBePresentInElementValue(expressionInput, expression)));
+        return this;
+    }
+
+    public String getCalculationResult() {
+        return expressionInput.getAttribute("value");
+    }
+
+    public HomePage clearExpressionInput() {
+        expressionInput.clear();
+        return this;
+    }
+
+    public HomePage clickHistoryDropdown() {
+        historyDropdown.click();
+        return this;
+    }
+
+    public List<String> getCalculationHistoryList() {
+        return historyRecordsList.stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/test/CalculatorTest.java
+++ src/test/java/test/CalculatorTest.java
@@ -1,13 +1,63 @@
 package test;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 import page.HomePage;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class CalculatorTest extends CommonConditions {
 
     @Test
     public void shouldBeAbleToSeePerformedCalculationHistory() {
+        String firstExpression = "35*999+(100/4)";
+        String secondExpression = "cos(pi)";
+        String thirdExpression = "sqrt(81)";
+
+        List<String> expressionsList = Arrays.asList(firstExpression, secondExpression, thirdExpression);
+
+        String expectedFirstCalculationResult = "34990";
+        String expectedSecondCalculationResult = "-1";
+        String expectedThirdCalculationResult = "9";
+
         HomePage homePage = new HomePage(driver);
-        homePage.openPage();
+
+        String firstCalculationResult = homePage
+                .openPage()
+                .waitForPageLoad()
+                .consentManagementPlatformLoading()
+                .consentPersonalDataUsage()
+                .inputExpression(firstExpression)
+                .clickEqualSignButton()
+                .waitForExpressionInputValueUpdate()
+                .getCalculationResult();
+        assertEquals(firstCalculationResult, expectedFirstCalculationResult);
+
+        String secondCalculationResult = homePage
+                .clearExpressionInput()
+                .inputExpression(secondExpression)
+                .clickRadRadioButton()
+                .clickEqualSignButton()
+                .waitForExpressionInputValueUpdate()
+                .getCalculationResult();
+        assertEquals(secondCalculationResult, expectedSecondCalculationResult);
+
+        String thirdCalculationResult = homePage
+                .clearExpressionInput()
+                .inputExpression(thirdExpression)
+                .clickEqualSignButton()
+                .waitForExpressionInputValueUpdate()
+                .getCalculationResult();
+        assertEquals(thirdCalculationResult, expectedThirdCalculationResult);
+
+        List<String> calculationHistoryList = homePage
+                .clickHistoryDropdown()
+                .getCalculationHistoryList();
+        Collections.reverse(calculationHistoryList);
+
+        assertEquals(calculationHistoryList, expressionsList);
     }
 }
