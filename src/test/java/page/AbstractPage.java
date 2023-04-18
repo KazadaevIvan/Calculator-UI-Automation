489574739ef6f7f5a0cef3034ecdcf841c114625
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-18 11:41:09 -0700

    Add WAIT_TIMEOUT_SECONDS

--- src/test/java/page/AbstractPage.java
+++ src/test/java/page/AbstractPage.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractPage {
     protected WebDriver driver;
-
-    protected abstract AbstractPage openPage();
+    protected final int WAIT_TIMEOUT_SECONDS = 10;
 
     protected AbstractPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    protected abstract AbstractPage openPage();
 }
