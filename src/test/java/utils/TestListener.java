d67052f2c325ae99b1d744fe7a1cf5c1159f1c2c
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-19 00:30:14 -0700

    Add reporting

---
+++ src/test/java/utils/TestListener.java
@@ -0,0 +1,79 @@
+package utils;
+
+import driver.DriverSingleton;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.printf("================= STARTING TEST %s =================%n", iTestResult.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.printf("================= FINISHED TEST %s Duration: %ss =================%n", iTestResult.getName(),
+                getExecutionTime(iTestResult));
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.printf("================= FAILED TEST %s Duration: %ss =================%n", iTestResult.getName(),
+                getExecutionTime(iTestResult));
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.printf("================= SKIPPING TEST %s =================%n", iTestResult.getName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+
+    private void saveScreenshot(){
+        File screenCapture = ((TakesScreenshot) DriverSingleton
+                .getDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenCapture, new File(
+                    ".//target/screenshots/"
+                            + getCurrentTimeAsString() +
+                            ".png"));
+        } catch (IOException e) {
+            System.out.println("Failed to save screenshot: " + e.getLocalizedMessage());
+        }
+    }
+
+    private String getCurrentTimeAsString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd_HH-mm-ss" );
+        return ZonedDateTime.now().format(formatter);
+    }
+
+    private long getExecutionTime(ITestResult iTestResult) {
+        return TimeUnit.MILLISECONDS.toSeconds(iTestResult.getEndMillis() - iTestResult.getStartMillis());
+    }
+}
