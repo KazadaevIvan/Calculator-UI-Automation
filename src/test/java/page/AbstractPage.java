fcfb2f8798a391627f33f823c6c80197045de3b3
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-19 04:30:10 -0700

    Add logger

--- src/test/java/page/AbstractPage.java
+++ src/test/java/page/AbstractPage.java
@@ -1,13 +1,17 @@
 package page;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractPage {
     protected WebDriver driver;
+    protected Logger log;
     protected final int WAIT_TIMEOUT_SECONDS = 10;
 
     protected AbstractPage(WebDriver driver) {
         this.driver = driver;
+        log = LogManager.getRootLogger();
     }
 
     protected abstract AbstractPage openPage();
