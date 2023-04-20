e23a715ec8470a904c9817c3c70d3be4d0a92acf
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-20 00:51:03 -0700

    Removed test data from test

--- src/test/java/test/CalculatorTest.java
+++ src/test/java/test/CalculatorTest.java
@@ -1,29 +1,17 @@
 package test;
 
-
 import org.testng.annotations.Test;
-import steps.HomePageSteps;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static constants.Constant.TestData.*;
 
 public class CalculatorTest extends CommonConditions {
 
     @Test
     public void shouldBeAbleToSeePerformedCalculationHistory() {
-        String firstExpression = "35*999+(100/4)";
-        String secondExpression = "cos(pi)";
-        String thirdExpression = "sqrt(81)";
-
         List<String> expressionsList = Arrays.asList(firstExpression, secondExpression, thirdExpression);
-
-        String expectedFirstCalculationResult = "34990";
-        String expectedSecondCalculationResult = "-1";
-        String expectedThirdCalculationResult = "9";
-
-        HomePageSteps homePageSteps = new HomePageSteps(driver)
-                .openPage()
-                .consentManagementPlatformLoadingAndPersonalDataUsage();
 
         homePageSteps
                 .inputExpression(firstExpression)
