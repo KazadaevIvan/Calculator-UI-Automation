9fe3bce78a055045e732dc1c0c30c73f3df59655
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-20 00:48:27 -0700

    Add cookies

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -5,16 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import steps.HomePageSteps;
 import utils.TestListener;
+
+import static constants.Constant.Cookies.consentManagementPlatformLoadingCookie;
+import static constants.Constant.Cookies.consentPersonalDataUsageCookie;
 
 @Listeners({TestListener.class})
 public class CommonConditions {
-
     protected WebDriver driver;
+    protected HomePageSteps homePageSteps;
 
     @BeforeMethod
     public void setUp() {
         driver = DriverSingleton.getDriver();
+        homePageSteps = new HomePageSteps(driver);
+        driver.manage().addCookie(consentManagementPlatformLoadingCookie);
+        driver.manage().addCookie(consentPersonalDataUsageCookie);
+        driver.navigate().refresh();
     }
 
     @AfterMethod(alwaysRun = true)
