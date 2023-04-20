458711a98292b599dd4d597f77ba874e8f0a8d26
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-20 01:21:59 -0700

    Add @Step

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -1,6 +1,7 @@
 package test;
 
 import driver.DriverSingleton;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,10 +13,12 @@ import static constants.Constant.Cookies.consentManagementPlatformLoadingCookie;
 import static constants.Constant.Cookies.consentPersonalDataUsageCookie;
 
 @Listeners({TestListener.class})
-public class CommonConditions {
+public class
+CommonConditions {
     protected WebDriver driver;
     protected HomePage homePage;
 
+    @Step("Open browser and navigate to Home page")
     @BeforeMethod
     public void setUp() {
         driver = DriverSingleton.getDriver();
@@ -25,6 +28,7 @@ public class CommonConditions {
         driver.navigate().refresh();
     }
 
+    @Step("Close browser")
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         DriverSingleton.closeDriver();
