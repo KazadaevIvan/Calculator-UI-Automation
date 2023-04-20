beaa3b0ec29d20835b26f819862bb601b3df3471
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-04-20 00:49:06 -0700

    Removed empty line

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverSingleton {
-
     private static WebDriver driver;
 
     private DriverSingleton() {}
